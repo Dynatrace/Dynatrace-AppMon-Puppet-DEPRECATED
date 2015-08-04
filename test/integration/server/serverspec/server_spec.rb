693c1e056b66a9344b02d2ccb6ed42bf9c0374fd
Martin Etmajer <m.etmajer@gmail.com>
2015-08-04 16:24:39 +0200

    Remove unused connection testing of Performance Warehouse.

--- test/integration/server/serverspec/server_spec.rb
+++ test/integration/server/serverspec/server_spec.rb
@@ -73,27 +73,3 @@ end
 describe port(8021) do
   it { should be_listening }
 end
-
-=begin
-describe 'Dynatrace Server Performance Warehouse Configuration' do
-  it 'server should should respond with correct configuration' do
-    uri = URI('http://localhost:8020/rest/management/pwhconnection/config')
-    http = Net::HTTP.new(uri.host, uri.port)
-
-    request = Net::HTTP::Get.new(uri, {'Accept' => 'application/json'})
-    request.basic_auth 'admin', 'admin'
-    response = http.request(request)
-
-    expect(response.code).to eq('200')
-
-    data = JSON.parse(response.body)
-    expect(data['pwhconnectionconfiguration']['host']).to eq('localhost')
-    expect(data['pwhconnectionconfiguration']['port']).to eq('5432')
-    expect(data['pwhconnectionconfiguration']['dbms']).to eq('postgresql')
-    expect(data['pwhconnectionconfiguration']['dbname']).to eq('dynatrace-pwh')
-    expect(data['pwhconnectionconfiguration']['user']).to eq('dynatrace')
-    expect(data['pwhconnectionconfiguration']['password']).to eq('*********')
-    expect(data['pwhconnectionconfiguration']['usessl']).to eq(false)
-  end
-end
-=end
