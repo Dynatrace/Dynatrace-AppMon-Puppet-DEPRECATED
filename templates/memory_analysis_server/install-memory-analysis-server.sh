Dynatrace/Dynatrace-AppMon-Puppet-DEPRECATED

commit a0eb0a9e924f423f75bee9a7eeecc648cc9d8842

[JLT-165239] Kitchen tests of Dynatrace-Puppet scripts with Dynatrace 6.5 * Fixed server-collector-agent test + simplified installation process of Dynatrace packages

templates/memory_analysis_server/install-memory-analysis-server.sh
@@ -2,12 +2,7 @@
 cd <%= @installer_prefix_dir %>
 
 # run the installer, capture its STDOUT and exit status
-installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> <<STDIN
-<%= @installer_bitsize %>
-Y
-Y
-Y
-STDIN`
+installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> -b %= @installer_bitsize %> -y`
 result=$?
 
 # fetch an arbitrary init script name from the installer STDOUT

templates/agents_package/install-agents-package.sh
@@ -2,11 +2,7 @@
 cd <%= @installer_prefix_dir %>
 
 # run the installer, capture its STDOUT and exit status
-installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> <<STDIN
-<%= @installer_bitsize %>
-Y
-STDIN`
-
+installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> -b <%= @installer_bitsize %> -y`
 result=$?
 
 # fetch an arbitrary init script name from the installer STDOUT

templates/collector/install-collector.sh
@@ -2,12 +2,7 @@
 cd <%= @installer_prefix_dir %>
 
 # run the installer, capture its STDOUT and exit status
-installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> <<STDIN
-<%= @installer_bitsize %>
-Y
-Y
-Y
-STDIN`
+installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> -b <%= @installer_bitsize %> -y`
 result=$?
 
 # fetch an arbitrary init script name from the installer STDOUT

templates/server/install-server.sh
@@ -2,12 +2,7 @@
 cd <%= @installer_prefix_dir %>
 
 # run the installer, capture its STDOUT and exit status
-installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> <<STDIN
-<%= @installer_bitsize %>
-Y
-Y
-Y
-STDIN`
+installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> -b <%= @installer_bitsize %> -y`
 result=$?
 
 # fetch an arbitrary init script name from the installer STDOUT

commit 75a723d0aaec6eb8b3b055068359d2d55eacc867

[JLT-165239] Kitchen tests of Dynatrace-Puppet scripts with Dynatrace 6.5 * Fixed memory-analysis-server (bug introduced in a0eb0a9e92)

templates/memory_analysis_server/install-memory-analysis-server.sh
@@ -2,7 +2,7 @@
 cd <%= @installer_prefix_dir %>
 
 # run the installer, capture its STDOUT and exit status
-installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> -b %= @installer_bitsize %> -y`
+installer_output=`java -jar <%= @installer_cache_dir %>/<%= @installer_file_name %> -y`
 result=$?
 
 # fetch an arbitrary init script name from the installer STDOUT